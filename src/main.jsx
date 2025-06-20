948aad0ec82470f024e3e41c770b8d9157db7fcd
alessiosch <alessiosch@gmail.com>
2025-06-20 17:05:33 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppointmentScheduler from "./components/AppointmentScheduler";
-import "./index.css";
+import AppointmentScheduler from "./AppointmentScheduler";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
